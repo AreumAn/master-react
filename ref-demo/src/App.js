984ac05ef10a8060f14fe92507bbf0dc7b6a1aee
AREUM AN <areum.an.0928@gmail.com>
2020-02-07 00:11:27 -0500

    Add validation with state, class

--- ref-demo/src/App.js
+++ ref-demo/src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import Validation from 'Validation';
 
 function App() {
     return (
-        <div></div>
+        <div>
+            <Validation></Validation>
+        </div>
     );
 }
 
---
+++ ref-demo/src/Validation.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import 'Validation.css';
+
+class Validation extends Component {
+    state = {
+        password: '',
+        clicked: false,
+        Validated: false
+    }
+
+    handleChange = (e) => {
+        this.setState({ password: e.target.value });
+    }
+
+    handleButtonClick = () => {
+        this.setState({ 
+            clicked: true,
+            Validated: this.state.password === '0000'
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <input
+                    type="password"
+                    value={this.state.password}
+                    onChange={this.handleChange}
+                    className={this.state.clicked ? (this.state.Validated ? 'sucess' : 'failure') : '' } />
+                    <button onClick={this.handleButtonClick}>Check Validation</button>
+            </div>
+        );
+    }
+}
+
+export default Validation;
