510e844caccb3d8a78d22359e09cfe2d2e8183d8
AREUM AN <areum.an.0928@gmail.com>
2020-02-11 01:23:10 -0500

    Add API test

--- movie-viewer/src/App.js
+++ movie-viewer/src/App.js
@@ -1,8 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 const App = () => {
+    const [data, setData] = useState(null);
+    const onClick = async() => {
+        try {
+            const response = await axios.get(
+                'https://api.themoviedb.org/3/movie/550?api_key=94abad8b6e42cfebed4f0232a57158d4',
+            );
+            setData(response.data);
+        } catch(e) {
+            console.log(e);
+        }
+    };
+
     return (
-        <div>Movie site</div>
+        <div>
+            <div>
+                <button onClick={onClick}>Get Data</button>
+            </div>
+            { data && <textarea rows={7} value={JSON.stringify(data, null, 2)}/> }
+        </div>
     );
 }
 
