c0c760ea173f0aad23ec34b624bf02a1762b6251
AREUM AN <areum.an.0928@gmail.com>
2020-02-18 23:31:39 -0500

    Add movie detail style

--- movie-viewer/src/components/MovieDetail.js
+++ movie-viewer/src/components/MovieDetail.js
@@ -4,9 +4,65 @@ import { moviesApi } from 'api';
 
 const Container = styled.div`
     height: calc(100vh - 50px);
-    width: 100%;
     position: relative;
     padding: 50px;
+`;
+
+const Backdrop = styled.div`
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-image: url(${props => props.bgImage});
+    background-position: center center;
+    background-size: cover;
+    filter: blur(3px);
+    opacity: 0.5;
+    z-index: 0;
+`;
+
+const Content = styled.div`
+    display: flex;
+    width: 100%;
+    position: relative;
+    z-index: 1;
+    height: 100%;
+`;
+
+const Cover = styled.div`
+    width: 30%;
+    background-image: url(${props => props.bgImage});
+    background-position: center center;
+    background-size: cover;
+    height: 100%;
+    border-radius: 5px;
+`;
+
+const Data = styled.div`
+    width: 70%;
+    margin-left: 10px;
+`;
+
+const Title = styled.h3`
+    font-size: 32px;
+`;
+
+const ItemContainer = styled.div`
+    margin: 20px 0;
+`;
+
+const Item = styled.span``;
+
+const Divider = styled.span`
+    margin: 0 10px;
+`;
+
+const Overview = styled.p`
+    font-size: 12px;
+    opacity: 0.7;
+    line-height: 1.5;
+    width: 50%;
 `;
 
 
@@ -41,7 +97,51 @@ const MovieDetail = ({ pathMovieId }) => {
     }
 
     return (
-        <div>hey hey hye</div>
+        <>
+            <Container>
+                <title>
+                {movie.original_title ? movie.original_title : movie.original_name}{" "}
+                </title>
+                <Backdrop
+                    bgImage={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`}
+                />
+                <Content>
+                    <Cover
+                        bgImage={
+                            movie.poster_path && `https://image.tmdb.org/t/p/original${movie.poster_path}`
+                        }
+                    />
+                    <Data>
+                        <Title>
+                            {movie.original_title
+                            ? movie.original_title
+                            : movie.original_name}
+                        </Title>
+                        <ItemContainer>
+                            <Item>
+                            {movie.release_date
+                                ? movie.release_date.substring(0, 4)
+                                : movie.first_air_date.substring(0, 4)}
+                            </Item>
+                            <Divider>•</Divider>
+                            <Item>
+                            {movie.runtime ? movie.runtime : movie.episode_run_time[0]} min
+                            </Item>
+                            <Divider>•</Divider>
+                            <Item>
+                            {movie.genres &&
+                                movie.genres.map((genre, index) =>
+                                index === movie.genres.length - 1
+                                    ? genre.name
+                                    : `${genre.name} / `
+                                )}
+                            </Item>
+                        </ItemContainer>
+                        <Overview>{movie.overview}</Overview>
+                    </Data>
+                </Content>
+            </Container>
+        </>
     )
 
 }
