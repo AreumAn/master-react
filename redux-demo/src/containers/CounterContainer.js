edb86b423ff6975cab0b185a7b01beaccde41296
AREUM AN <areum.an.0928@gmail.com>
2020-02-22 17:30:01 -0500

    Refactor connect

--- redux-demo/src/containers/CounterContainer.js
+++ redux-demo/src/containers/CounterContainer.js
@@ -7,16 +7,12 @@ const CounterContainer = ({ number, increase, decrease }) => {
     return <Counter number={number} onIncrease={increase} onDecrease={decrease} />;
 };
 
-const mapStateToProps = state => ({
-    number: state.counter.number,
-});
-
-const mapDispatchToProps = dispatch => ({
-    increase: () => dispatch(increase()),
-    decrease: () => dispatch(decrease())
-});
-
 export default connect(
-    mapStateToProps,
-    mapDispatchToProps,
+    state => ({
+        number: state.counter.number,
+    }),
+    {
+        increase,
+        decrease,
+    },
 )(CounterContainer);
