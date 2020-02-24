2d0571c4158a91b17a5f3cee8610b3997759b538
AREUM AN <areum.an.0928@gmail.com>
2020-02-24 18:05:06 -0500

    Add redux-thunk

--- redux-middleware/src/index.js
+++ redux-middleware/src/index.js
@@ -5,9 +5,10 @@ import rootReducer from './modules';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import { createLogger } from 'redux-logger';
+import ReduxThunk from 'redux-thunk';
 
 const logger = createLogger();
-const store = createStore(rootReducer, applyMiddleware(logger));
+const store = createStore(rootReducer, applyMiddleware(logger, ReduxThunk ));
 
 ReactDOM.render(<Provider store={store}>
                     <App />
