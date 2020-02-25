3ec5db0f4b337bc5ddd0cfb118e8d3d0a5065a28
AREUM AN <areum.an.0928@gmail.com>
2020-02-25 01:21:26 -0500

    Add redux-saga with API

--- redux-middleware/src/index.js
+++ redux-middleware/src/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import rootReducer from './modules';
+import rootReducer, { rootSage } from './modules';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import { createLogger } from 'redux-logger';
 import ReduxThunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import createSagaMiddleware from 'redux-saga';
 
 const logger = createLogger();
-const store = createStore(rootReducer, applyMiddleware(logger, ReduxThunk ));
+const sagaMiddleware = createSagaMiddleware();
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(logger, ReduxThunk, sagaMiddleware )));
+sagaMiddleware.run(rootSage);
 
 ReactDOM.render(<Provider store={store}>
                     <App />
--- redux-middleware/src/modules/index.js
+++ redux-middleware/src/modules/index.js
@@ -1,12 +1,17 @@
 import { combineReducers } from 'redux';
 import counter from './counter';
-import sample from './sample';
+import sample, { sampleSaga } from './sample';
 import loading from './loading';
+import { all } from 'redux-saga/effects';
 
 const rootReducer = combineReducers({
     counter,
     sample,
     loading
 });
+
+export function* rootSage() {
+    yield all([sampleSaga()]);
+}
 
 export default rootReducer;
